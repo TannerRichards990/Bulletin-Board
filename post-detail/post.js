486cdb1915f1046fa4978ff62ccb2832142e1bc5
Tanner Richards <Tanner.richards990@gmail.com>
2022-08-16 12:25:07 -0700

    imported render and fetch utils into my post detail js file, wrote out async function for display post detail.

--- post-detail/post.js
+++ post-detail/post.js
@@ -1,3 +1,14 @@
-import { renderPostDetail } from "../render-utils";
+import { renderPostDetail } from '../render-utils.js';
+import { getPostsById } from '../fetch-utils.js';
 
-const postDetail
+const postDetailEl = document.getElementById('post-detail');
+
+const params = new URLSearchParams(window.location.search);
+
+async function displayPostDetail() {
+    const post = await getPostsById(params.get('id'));
+    const postEl = renderPostDetail(post);
+    postDetailEl.append(postEl);
+}
+
+displayPostDetail();
