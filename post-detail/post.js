a414ef8c4c79e3ab6755021c62845e90d884b536
Tanner Richards <Tanner.richards990@gmail.com>
2022-08-17 14:41:58 -0700

    added await to the delete button EL, when delete is clicked on the post, the post is now removed by the user, and the user is returned to the home page.

--- post-detail/post.js
+++ post-detail/post.js
@@ -1,5 +1,5 @@
 import { renderPostDetail } from '../render-utils.js';
-import { checkAuth, deletePostById, getPostsById, getUser } from '../fetch-utils.js';
+import { checkAuth, deletePostById, getPostsById, } from '../fetch-utils.js';
 
 const postDetailEl = document.getElementById('post-detail');
 const user = checkAuth();
@@ -13,8 +13,10 @@ async function displayPostDetail() {
     if (user.id === post.user_id) {
         const buttonEl = document.createElement('button');
         buttonEl.textContent = 'delete';
-        buttonEl.addEventListener('click', () => {
-            deletePostById(post.id);
+        buttonEl.classList.add('post');
+        buttonEl.addEventListener('click', async () => {
+            await deletePostById(post.id);
+            location.replace(`../`);
             
         });
         postDetailEl.append(buttonEl);
