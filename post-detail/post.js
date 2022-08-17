215ce8d57e8fa15078593f6f19395754645f192b
Tanner Richards <Tanner.richards990@gmail.com>
2022-08-17 12:35:36 -0700

    added the delete button to the user.id so they can delete a post

--- post-detail/post.js
+++ post-detail/post.js
@@ -1,7 +1,8 @@
 import { renderPostDetail } from '../render-utils.js';
-import { getPostsById } from '../fetch-utils.js';
+import { checkAuth, deletePostById, getPostsById, getUser } from '../fetch-utils.js';
 
 const postDetailEl = document.getElementById('post-detail');
+const user = checkAuth();
 
 const params = new URLSearchParams(window.location.search);
 
@@ -9,6 +10,15 @@ async function displayPostDetail() {
     const post = await getPostsById(params.get('id'));
     const postEl = renderPostDetail(post);
     postDetailEl.append(postEl);
+    if (user.id === post.user_id) {
+        const buttonEl = document.createElement('button');
+        buttonEl.textContent = 'delete';
+        buttonEl.addEventListener('click', () => {
+            deletePostById(post.id);
+            
+        });
+        postDetailEl.append(buttonEl);
+    }
 }
 
 displayPostDetail();
